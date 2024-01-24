2825173f27bc026d7053c99cdd79481864e30f2e
TryToCompile <adam1999@tut.by>
2024-01-25 01:46:33 +0300

    better

--- script.js
+++ script.js
@@ -4,8 +4,7 @@
 Необходимо реализовать следующее: 
 1) +
 
-2) Объект от класса WheatherWidget должен уметь запрашивать данные о погоде с http://openweathermap.org. По умолчанию запрос делать для Минска 
-(для улучшения, можно сделать по определению местоположения и запросу по координатам), на русском.
+2) +
 
 3) +
 4) +
@@ -14,7 +13,7 @@
 затем отфильтровать ее, и забрать информацию о погоде на сегодня, завтра, послезавтра примерно на 12 часов дня. Пока обрабатываются данные показать анимированный лоадер (можно просто gif картинку, 
 но желательно какую-нибудь svg-анимацию).
 7) Необходимо, чтобы WheatherWidget был совершенно независимый модуль/класс, т.е. отдельный файл, который можно подключить к себе на страницу и запустить new WheatherWidget().getWeather().
-8) Использовать promise для обработки полученных данных (fetch или  async/await).
+8) +
  */
 
 
@@ -98,22 +97,85 @@ class WeatherWidget {
     })
   }
   print(data) {
+    // let weatherContainer = document.createElement('div');
+    // weatherContainer.innerHTML = '<div style="display: flex;"><p style="font-size: 20px; margin-bottom: auto; font-family: monospace;">Город '+data.name+': '+data.weather[0].description+
+    // ' с температурой аля ~'+Math.round(data.main.temp)+' °C'+                             
+    // '</p>'+'<img src="https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png" alt="weather icon"/>'+
+    // ' <span id="closeWeatherWindow" style="position: absolute; top: 0px; right: 11px; cursor: pointer; font-size: 20px; color: white;">&#10006;</span></div><hr/>'+
+    // '<div style="font-family: monospace; font-size: 40px;">Ветер: '+data.wind.speed+' м/с<br/>Влажность: '+data.main.humidity+'%'+
+    // '</div>'+
+    // '<button id="forecast-btn" style="font-family: monospace; font-size: 25px; width: 200px; margin: 13px 22%; border:none; border-radius: 10px;">Хочу прогноз на 3 дня!</button>';
+    // weatherContainer.style = 'position: absolute; top: 2%; right: 2%; word-wrap: break-word; width: 350px; color: white; border-radius: 20px; height: 300px; background-color: #818080; overflow:hidden; border: 1px solid #ccc; padding: 10px;';
+    // document.body.append(weatherContainer);
+
     let weatherContainer = document.createElement('div');
-    weatherContainer.innerHTML = '<div style="display: flex;"><p style="font-size: 20px; margin-bottom: auto; font-family: monospace;">Город '+data.name+': '+data.weather[0].description+
-    ' с температурой аля ~'+Math.round(data.main.temp)+' °C'+                             
-    '</p>'+'<img src="https://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png" alt="weather icon"/>'+
-    ' <span id="closeWeatherWindow" style="position: absolute; top: 0px; right: 11px; cursor: pointer; font-size: 20px; color: white;">&#10006;</span></div><hr/>'+
-    '<div style="font-family: monospace; font-size: 40px;">Ветер: '+data.wind.speed+' м/с<br/>Влажность: '+data.main.humidity+'%'+
-    '</div>'+
-    '<button id="forecast-btn" style="font-family: monospace; font-size: 25px; width: 200px; margin: 13px 22%; border:none; border-radius: 10px;">Хочу прогноз на 3 дня!</button>';
-    weatherContainer.style = 'position: absolute; top: 2%; right: 2%; word-wrap: break-word; width: 350px; color: white; border-radius: 20px; height: 300px; background-color: #818080; overflow:hidden; border: 1px solid #ccc; padding: 10px;';
-    document.body.append(weatherContainer);
-    document.getElementById('closeWeatherWindow').addEventListener('click', () => {
-      weatherContainer.remove();
-    })
-    document.getElementById('forecast-btn').addEventListener('click', () => {
+    weatherContainer.style.cssText = 'position: absolute; top: 2%; right: 2%; word-wrap: break-word; width: 350px; color: white; border-radius: 20px; height: 300px; background-color: #818080; overflow:hidden; border: 1px solid #ccc; padding: 10px;';
+
+    let titleDiv = document.createElement('div');
+    titleDiv.style.cssText = 'display: flex';
+  
+    let title = document.createElement('p');
+    title.style.cssText = 'font-size: 20px; margin-bottom: auto; font-family: monospace'; 
+    title.textContent = `Город ${data.name}: ${data.weather[0].description} с температурой аля ~${Math.round(data.main.temp)} °C`;
+  
+    let icon = document.createElement('img');
+    icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    icon.alt = 'weather icon';
+  
+    let closeButton = document.createElement('span');
+    closeButton.id = 'closeWeatherWindow';
+    closeButton.style.cssText = 'position: absolute; top: 0px; right: 11px; cursor: pointer; font-size: 20px; font-family:cursive; color: white';
+    closeButton.textContent = 'x';
+  
+    titleDiv.appendChild(title); 
+    titleDiv.appendChild(icon);
+  
+    weatherContainer.appendChild(titleDiv);
+    weatherContainer.appendChild(closeButton);
+    
+    let hr = document.createElement('hr');
+
+    let detailsDiv = document.createElement('div');
+    detailsDiv.style.cssText = 'font-family: monospace; font-size: 40px;';
+  
+    let wind = document.createElement('div');
+    wind.textContent = `Ветер: ${data.wind.speed} м/с`;
+  
+    let humidity = document.createElement('div'); 
+    humidity.textContent = `Влажность: ${data.main.humidity}%`;
+  
+    detailsDiv.appendChild(wind);
+    detailsDiv.appendChild(humidity);
+  
+    weatherContainer.appendChild(hr);
+    weatherContainer.appendChild(detailsDiv);
+  
+    let forecastButton = document.createElement('button');
+    forecastButton.id = 'forecast-btn';
+    forecastButton.style.cssText = 'background-color: #97f6ff; font-family: monospace; font-size: 25px; width: 200px; margin: 13px 22%; border:none; border-radius: 10px; transition: 0.3s;';
+    forecastButton.textContent = 'Хочу прогноз на 3 дня!';
+  
+    weatherContainer.appendChild(forecastButton);
+  
+    document.body.appendChild(weatherContainer);
+
+    forecastButton.addEventListener('mouseenter', () => {
+      forecastButton.style.backgroundColor = 'rgba(20, 80, 90, 0.5)';
+    });
+    
+    forecastButton.addEventListener('mouseleave', () => {
+      forecastButton.style.backgroundColor = '#97f6ff';
+    });
+
+    forecastButton.addEventListener('click', () => {
       this.getForecast();
-    })
+    });
+
+    closeButton.addEventListener('click', () => {
+      weatherContainer.remove();
+    });
+
+
   }
   getForecast() {
     
